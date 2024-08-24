7065f209ec000448c2cd57f49264a56c965d9001
JeffMiller <42790830+JeffMillerAuthor@users.noreply.github.com>
2024-08-24 12:17:32 -0500

    Update dbo.Hospital.Table.sql

--- SQL Code/dbo.Hospital.Table.sql
+++ SQL Code/dbo.Hospital.Table.sql
@@ -12,6 +12,3 @@ CREATE TABLE [dbo].[Hospital](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 
-INSERT [dbo].[Hospital] ([ClinicID], [ClinicName], [City], [State], [Zip]) VALUES (N'553', N'WeCare Care Clinic', N'Fort Worth', N'TX', N'76036')
-INSERT [dbo].[Hospital] ([ClinicID], [ClinicName], [City], [State], [Zip]) VALUES (N'79', N'Prosper Health Clinic', N'Plano', N'TX', N'75025')
-INSERT [dbo].[Hospital] ([ClinicID], [ClinicName], [City], [State], [Zip]) VALUES (N'876', N'Legacy Health', N'Austin', N'TX', N'78702')
