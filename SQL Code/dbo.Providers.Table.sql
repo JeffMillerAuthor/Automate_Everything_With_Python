ed98968dfc57c09a0257ba1e00d4036f2c24eba3
JeffMiller <42790830+JeffMillerAuthor@users.noreply.github.com>
2024-08-24 12:19:30 -0500

    Update dbo.Providers.Table.sql

--- SQL Code/dbo.Providers.Table.sql
+++ SQL Code/dbo.Providers.Table.sql
@@ -10,6 +10,3 @@ CREATE TABLE [dbo].[Providers](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 
-INSERT [dbo].[Providers] ([ProviderID], [ProviderName], [Phone]) VALUES (N'6015', N'Baker, Sara', N'385-555-0145')
-INSERT [dbo].[Providers] ([ProviderID], [ProviderName], [Phone]) VALUES (N'6350', N'Zhou, Jon', N'688-555-0128')
-INSERT [dbo].[Providers] ([ProviderID], [ProviderName], [Phone]) VALUES (N'6541', N'Carson, Kevin', N'477-555-0181')
